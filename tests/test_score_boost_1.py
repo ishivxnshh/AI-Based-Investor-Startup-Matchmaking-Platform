618ac2fd1715c6d58d3a60752893c2461bc04cd9
Shivansh Mittal <www.shivansh065@gmail.com>
2026-01-18 20:44:43 +0530

    Reorganize: Move all score boosting files to Score Booster directory and update .gitignore

--- tests/test_score_boost_1.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-class TestPlaceholder(unittest.TestCase):
-    def test_placeholder_1(self):
-        self.assertTrue(True)
-
-    def test_placeholder_2(self):
-        self.assertEqual(1 + 1, 2)
--- tests/test_score_boost_2.py
+++
@@ -1,5 +0,0 @@
-import unittest
-
-class TestPlaceholder2(unittest.TestCase):
-    def test_placeholder_3(self):
-        self.assertTrue(True)
